2062f46f83d479ce8b0d45eec367268b0151c743
Joel Boci <126195254+JoelBoci@users.noreply.github.com>
2026-03-03 21:42:08 +0000

    Added config to save the theme that was applied before closing so that it applies the next time the Notepad is launched

---
+++ src/com/notepad/config/AppConfig.java
@@ -0,0 +1,29 @@
+package com.notepad.config;
+
+import com.notepad.main.Notepad;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.prefs.BackingStoreException;
+import java.util.prefs.Preferences;
+
+
+public final class AppConfig {
+    private static final Preferences PREFS = Preferences.userRoot().node("com/notepad"); // use a path style
+    private static final Logger mLogger = LoggerFactory.getLogger(Notepad.class);
+
+    private AppConfig() {}
+
+    public static void setTheme(String theme) {
+        PREFS.put("theme", theme);
+        try {
+            PREFS.flush();
+        } catch (BackingStoreException e) {
+            mLogger.info("Theme setting failed: " + e.getMessage());
+        }
+    }
+
+    public static String getTheme() {
+        return PREFS.get("theme", "light");
+    }
+}
---
+++ src/com/notepad/config/ThemeManager.java
@@ -0,0 +1,22 @@
+package com.notepad.config;
+
+import com.formdev.flatlaf.FlatDarkLaf;
+import com.formdev.flatlaf.FlatDarculaLaf;
+import com.formdev.flatlaf.FlatLightLaf;
+import com.formdev.flatlaf.themes.FlatMacDarkLaf;
+import com.formdev.flatlaf.themes.FlatMacLightLaf;
+
+public final class ThemeManager {
+    private ThemeManager() {}
+
+    public static void apply(String themeKey) {
+        switch (themeKey) {
+            case "light" -> FlatLightLaf.setup();
+            case "dark" -> FlatDarkLaf.setup();
+            case "darcula" -> FlatDarculaLaf.setup();
+            case "macLight" -> FlatMacLightLaf.setup();
+            case "macDark" -> FlatMacDarkLaf.setup();
+            default -> FlatLightLaf.setup();
+        }
+    }
+}
